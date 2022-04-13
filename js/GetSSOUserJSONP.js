2cbdbaf9fe68fdd40a615a71f3c902eb2f09a0e2
sunnie lu <lufeiya119@qq.com>
2022-04-13 10:19:50 +0800

    ++

--- js/GetSSOUserJSONP.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-type: "get",
-dataType: "jsonp",
-jsonpCallback: "callback",
-url: "https://sso.31huiyi.com/ssouser/GetSSOUserByJSONP",
-data:"jsoncallback=callback",
-success: function (json) {ssoUserCallback(json)},
-error: function (e) {console.log(e)}
-});
