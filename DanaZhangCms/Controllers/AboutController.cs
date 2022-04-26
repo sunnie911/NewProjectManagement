7a22d060b01e8028646009063933d29af848ac04
sunnie lu <lufeiya119@qq.com>
2022-04-26 18:09:41 +0800

    add png

---
+++ DanaZhangCms/Controllers/AboutController.cs
@@ -0,0 +1,34 @@
+using DanaZhangCms.Core.Attributes;
+using DanaZhangCms.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+namespace DanaZhangCms
+{
+    [Ignore]
+    public class AboutController : BaseController
+    {
+
+        private IProductCategoryRepository _cateRepository;
+        private IProductRepository _proRepository;
+        private IContentsRepository _repository;
+        public AboutController(IContentsRepository repository,IProductRepository proRepository, IProductCategoryRepository cateRepository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _cateRepository = cateRepository;
+            _proRepository = proRepository;
+        }
+
+        public  IActionResult Index(string spellname = "")
+        {
+            var position = _proRepository.ToList();
+            ViewBag.ProductList = position;
+            ViewBag.CategoryList = _cateRepository.ToList();
+            var model =  _repository.FirstOrDefault(o => o.SpellName == spellname);
+            return View(model);
+        }
+
+
+    }
+}
